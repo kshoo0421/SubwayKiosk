af60ebab78aaa924e4d2aac48250944b62138b13
ky107601 <104256486+ky107601@users.noreply.github.com>
2024-10-09 18:05:09 +0900

    가격, 메뉴 상세 정보 리스트로 저장

--- SubwayKiosk/SQLManager.cpp
+++ SubwayKiosk/SQLManager.cpp
@@ -1,7 +1,7 @@
 #include"SQLManager.h"
-#include <QtSql>
 #include <QDebug>
-#include <QString>
+#include <QList>
+
 using namespace std;
 
 SQLManager::SQLManager()
@@ -22,31 +22,76 @@ SQLManager::SQLManager()
         qDebug()<<"connected";
     }
 
-
-    QSqlQuery query;
-    if (!query.exec("SELECT * FROM Price")) {
-        qDebug() << "Price query execution failed: " << query.lastError().text();
-    } else {
+     QSqlQuery query;
+    /* 리스트에 가격 정보 저장 */
+    if (query.exec("SELECT * FROM Price")) {// 쿼리 결과를 QList에 저장
         while (query.next()) {
-            // Price 테이블에서 데이터를 처리
-            int price15cm = query.value(0).toInt(); // 15cm 가격
-            int price30cm = query.value(1).toInt(); // 30cm 가격
-            qDebug() << "15cm Price:" << price15cm << ", 30cm Price:" << price30cm;
+            Price info;
+            info._15cm = query.value(0).toInt();  // 첫 번째 칼럼: 15cm 가격
+            info._30cm = query.value(1).toInt();  // 두 번째 칼럼: 30cm 가격
+            priceList.append(info);  // 리스트에 추가
         }
+    } else {
+        qDebug() << "Query execution failed: " << query.lastError().text();
+        return ;
+    }
+    /* 리스트에 메뉴 상세 정보 저장 */
+    if (query.exec("SELECT * FROM Detail")) {// 쿼리 결과를 QList에 저장
+        while (query.next()) {
+            Detail info;
+            info.kcal = query.value(0).toInt();  // 첫 번째 칼럼: 열량
+            info.protein = query.value(1).toFloat();  // 두 번째 칼럼: 단백질
+            info.saturatedFatty=query.value(2).toFloat(); // 세 번째 칼럼: 포화 지방
+            info.sugar=query.value(3).toFloat(); // 네 번째 칼럼: 당류
+            info.natrium = query.value(4).toInt(); // 다섯 번째 칼럼: 나트륨
+            info.expln = query.value(5).toString(); // 여섯 번째 칼럼: 상세 설명
+            detailList.append(info);  // 리스트에 추가
+        }
+    } else {
+        qDebug() << "Query execution failed: " << query.lastError().text();
+        return ;
     }
 
-    if (!query.exec("SELECT * FROM Detail")) {
-        qDebug() << "Details query execution failed: " << query.lastError().text();
-    } else {
-        while (query.next()) {
-            int kcal = query.value(0).toInt();
-            float protein = query.value(1).toFloat();
-            float satFat = query.value(2).toFloat();
-            float sugar = query.value(3).toFloat();
-            int nat = query.value(4).toInt();
-            QString expln = query.value(5).toString();
-            qDebug()<<"열량 : "<<kcal<<"kcal, 단백질 = "<<protein<<"g, 포화지방 : "<<satFat<<", 당류 : "<<sugar<<", 상세 설명 : "<<expln;
-        }
-    }
+    // /* DB 정상 로드 확인 */
+    // if (!this->query.exec("SELECT * FROM Price")) {
+    //     qDebug() << "Price query execution failed: " << this->query.lastError().text();
+    // } else {
+    //     while (this->query.next()) {
+    //         // Price 테이블에서 데이터를 처리
+    //         int price15cm = this->query.value(0).toInt(); // 15cm 가격
+    //         int price30cm = this->query.value(1).toInt(); // 30cm 가격
+    //         qDebug() << "15cm Price:" << price15cm << ", 30cm Price:" << price30cm;
+    //     }
+    // }
 
+    // if (!this->query.exec("SELECT * FROM Detail")) {
+    //     qDebug() << "Details query execution failed: " << this->query.lastError().text();
+    // } else {
+    //     while (this->query.next()) {
+    //         int kcal = this->query.value(0).toInt();
+    //         float protein = this->query.value(1).toFloat();
+    //         float satFat = this->query.value(2).toFloat();
+    //         float sugar = this->query.value(3).toFloat();
+    //         int nat = this->query.value(4).toInt();
+    //         QString expln = this->query.value(5).toString();
+    //         qDebug()<<"열량 : "<<kcal<<"kcal, 단백질 = "<<protein<<"g, 포화지방 : "<<satFat<<", 당류 : "<<sugar<<", 상세 설명 : "<<expln;
+    //     }
+    // }
+
+}
+
+int SQLManager::GetSandwichPrice(MainSandwich option, bool is15)
+{
+    int index = static_cast<int>(option);
+    Price p;
+    p= this->priceList.at(index);
+    if(is15)
+        return p._15cm;
+    else
+        return p._30cm;
+}
+SQLManager::Detail SQLManager::GetDetail(MainSandwich option)
+{
+    int index = static_cast<int>(option);
+    return this->detailList[index];
 }
--- SubwayKiosk/SQLManager.h
+++ SubwayKiosk/SQLManager.h
@@ -1,13 +1,30 @@
 #ifndef SQL_MAANGER_H
 #define SQL_MAANGER_H
 #include "Singleton.h"
-
+#include "enum.h"
+#include <QtSql>
+#include <QString>
 
 class SQLManager : public Singleton<SQLManager> {
     friend class Singleton<SQLManager>;
 private:
-    SQLManager();
+        struct Price{
+           int _15cm;
+           int _30cm;
+       };
+        struct Detail{
+            int kcal; //열량
+            float protein; //단백질(g)
+            float saturatedFatty; //포화지방(g)
+            float sugar; //당류(g)
+            int natrium; //나트륨(mg)
+            QString expln; //메뉴 설명
+        };
+        QList<Price> priceList; //가격  리스트
+        QList<Detail> detailList; //메뉴 상세 정보 리스트
+        SQLManager();
 public:
-
+    int GetSandwichPrice(MainSandwich option, bool is15) ; //샌드위치 가격 반환
+    SQLManager::Detail GetDetail(MainSandwich option); //샌드위치 상세 정보 반환
 };
 #endif
